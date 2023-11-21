7f35914fe17fecd599365df8744b371cb91bd963
Yvagaemiy <krylatiy2@mail.ru>
2023-11-21 14:51:31 +0300

    HW_002/2

--- 002_Simenar/hom_work/hw_002.py
+++ 002_Simenar/hom_work/hw_002.py
@@ -14,3 +14,27 @@ p = x * y
 print('сумма чисел X и Y = ',s)
 print('Произведение чисел X и Y = ',p)
 print(s,p)
+
+print('__Вариант с интернета_есть ошибка_______________')
+
+
+
+x = abs(int(input('Введите первое натуральное число X от 1 до 1000 ')))
+y = abs(int(input('Введите второе натуральное число Y от 1 до 1000 ')))
+if x < 1 or x > 1000 or y < 1 or y > 1000:
+    print('Вы ввели число не из заданного диапазона!')
+else:
+    S = x + y
+    P = x * y
+    stop = 0
+    for i in range(1001):
+        if stop != 1:
+            for j in range(1001):
+                if stop != 1:
+                    if i * j == P and i + j == S:
+                        print(i, j)
+                        stop = 1
+            else:
+                j = 1001
+        else:
+            i = 1001
