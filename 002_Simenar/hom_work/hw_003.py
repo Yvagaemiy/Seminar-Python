c42ae60e968738ceea52ece27beebac5c8b4c40e
Yvagaemiy <krylatiy2@mail.ru>
2023-11-21 14:49:22 +0300

    HW_003/2

--- 002_Simenar/hom_work/hw_003.py
+++ 002_Simenar/hom_work/hw_003.py
@@ -13,3 +13,20 @@ for i in  range(num):
     x = stepn ** i
     if x < num:
         print(x , end='-')
+        
+        
+print('__Вариант с интернета________________')
+
+N = abs(int(input('Введите число N: ')))
+stop = 0
+P = 2
+for i in range(N):
+    if stop != 1:
+        P = P ** i
+        if P <= N:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = N
